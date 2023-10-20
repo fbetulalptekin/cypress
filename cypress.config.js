326363a3b03156926385188c45660f44b18bd952
fbetulalptekin <f16btl@gmail.com>
2023-10-20 12:22:20 +0300

    day6

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '3ecfrp',
+  projectId: '3ecfrp', // cypress ile projeyi bagladiktan sonra buraya geldi
   viewportHeight:700,
   viewportWidth:1200,
   e2e: {
@@ -10,5 +10,9 @@ module.exports = defineConfig({
 
     },
     baseUrl:'http://www.google.com'
+    // video: false --> video kaydi alinmasini engeller
+    // retire: 2 --> fail olan testlerin iki kez daha calismasini saglar
+
+
   },
 });
